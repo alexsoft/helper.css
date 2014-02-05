7ae830f83090e7869d2005873c8057be2e8f3d8b
Alexey Plekhanov <oleksii.plekhanov@gmail.com>
2014-02-06 01:43:53 +0200

    make margins and paddings

---
+++ Gruntfile.js
@@ -0,0 +1,50 @@
+module.exports = function(grunt) {
+
+	// Project configuration.
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+
+		banner: '/*!\n' +
+				' * helper.css v<%= pkg.version %> (<%= pkg.homepage %>)\n' +
+				' * Copyright <%= grunt.template.today("yyyy") %> <%= pkg.author %>\n' +
+				' * Licensed under <%= pkg.license %>\n' +
+				' */',
+		less: {
+			dev: {
+				options: {
+					cleancss: false
+				},
+				files: {
+					"dist/helper.css": "less/helper.less"
+				}
+			},
+			min: {
+				options: {
+					banner: 'dafa',
+					cleancss: true
+				},
+				files: {
+					'dist/helper.min.css': 'dist/helper.css'
+				}
+			}
+		},
+		usebanner: {
+			dist: {
+				options: {
+					position: 'top',
+					banner: '<%= banner %>'
+				},
+				files: {
+					src: [
+						'dist/*.css'
+					]
+				}
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-less');
+	grunt.loadNpmTasks('grunt-banner');
+
+	grunt.registerTask('default', ['less', 'usebanner']);
+};
